aa0e52405e2d676948aae830f57ccfaec00b8536
yashthummar88 <yashthummar88@gmail.com>
2021-08-02 13:38:53 +0530

    Initial Commit

---
+++ lib/Screens/HomePageScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePageScreen extends StatefulWidget {
+  const HomePageScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomePageScreenState createState() => _HomePageScreenState();
+}
+
+class _HomePageScreenState extends State<HomePageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
